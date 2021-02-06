901fb3f056f7aced879f0761073d5b68aba6b49e
Lasette <lasette.brown@gmail.com>
2021-02-06 13:07:33 -0700

    pseudocode and file restructuring.

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+console.log("linked")
+//When the 'help me' button is clicked,
+//redirect to a new page
+//(optional) with a form asking for user preferences/perameters
+//(optional) use those parameters to limit our search
+//a random movie is generated
+//and displayed on the page with title, summery, and poster image of the film
+//and a random delivery restaurant is generated
+//and displayed on the page with restaurant title, description (and optional other things).
+
+//when the 'choose a new movie' button is clicked
+//Then a different random movie is generated
+//and replaces the movie displayed on the page.
+
+//when the 'choose a new restaurant' button is clicked
+//then a different restaurant is generated 
+//and replaces the restaurant displayed on the page.
+
+//When the 'save' button is clicked
+//then the currently displayed recommendations are saved into local storage.
+
+//When the 'history' button is clicked
+//then we are redirected to a third page
+//which displays past recommendations.
